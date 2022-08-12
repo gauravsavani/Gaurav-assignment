4b91169d4be980ce694b6581469617278631ce29
Gaurav Savani <gauravsavani0@gmail.com>
2022-08-12 13:03:11 +0530

    Database activity added

---
+++ Module 5/app/src/main/java/com/savani/modulefive/Database.kt
@@ -0,0 +1,11 @@
+package com.savani.modulefive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Database : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daatabase)
+    }
+}
